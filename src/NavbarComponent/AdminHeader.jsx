716978f2a851edfa89d939bd8e9392ff46c1fd77
ar-itio <arun@itio.in>
2024-06-05 12:15:59 +0530

    test

--- src/NavbarComponent/AdminHeader.jsx
+++ src/NavbarComponent/AdminHeader.jsx
@@ -3,7 +3,7 @@ import { Link, useNavigate } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlus, faEye, faEnvelope, faUserClock, faUsers, faMoneyCheckAlt, faCheckCircle, faTicketAlt } from "@fortawesome/free-solid-svg-icons";
+import { faListAlt, faEye, faEnvelope, faUserClock, faUsers, faMoneyCheckAlt, faCheckCircle, faTicketAlt } from "@fortawesome/free-solid-svg-icons";
 
 const AdminHeader = () => {
   let navigate = useNavigate();
@@ -31,57 +31,17 @@ const AdminHeader = () => {
 
   return (
     <ul className="navbar-nav ms-auto mb-2 mb-lg-0 me-5">
-      {/* <li className="nav-item">
+      <li className="nav-item card">
         <Link
-          to="/admin/fee/detail/add"
+          to="/home"
           className="nav-link active"
           aria-current="page"
         >
-          <FontAwesomeIcon icon={faPlus} className="icon" />
-          &nbsp;<b className="text-color">Add Fee</b>
-        </Link>
-      </li> */}
-      <li className="nav-item">
-        <Link
-          to="/admin/fee/detail/view"
-          className="nav-link active"
-          aria-current="page"
-        >
-          <FontAwesomeIcon icon={faEye} className="icon" />
-          &nbsp;<b className="text-color">View Fee Details</b>
+          <FontAwesomeIcon icon={faListAlt} className="icon" />&nbsp;
+          <span className="text-color"> <strong>Dashboard</strong></span>
         </Link>
       </li>
-      <li className="nav-item">
-        <Link
-          to="/admin/fee/detail/email"
-          className="nav-link active"
-          aria-current="page"
-        >
-          <FontAwesomeIcon icon={faEnvelope} className="icon" />
-          &nbsp;<b className="text-color">Email Templates</b>
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link
-          to="/admin/customer/pending"
-          className="nav-link active"
-          aria-current="page"
-        >
-          <FontAwesomeIcon icon={faUserClock} className="icon" />
-          &nbsp;<b className="text-color">Pending Customers</b>
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link
-          to="/admin/customer/pendingAccounts"
-          className="nav-link active"
-          aria-current="page"
-        >
-          <FontAwesomeIcon icon={faUserClock} className="icon" />
-          &nbsp;<b className="text-color">Pending  Accounts</b>
-        </Link>
-      </li>
-      <li className="nav-item">
+      <li className="nav-item card">
         <Link
           to="/admin/all/bank/customers"
           className="nav-link active"
@@ -91,17 +51,27 @@ const AdminHeader = () => {
           &nbsp;<b className="text-color">All Customers</b>
         </Link>
       </li>
-      <li className="nav-item">
+      <li className="nav-item card">
         <Link
-          to="/admin/customer/transaction/pending"
+          to="/admin/customer/pending"
           className="nav-link active"
           aria-current="page"
         >
-          <FontAwesomeIcon icon={faMoneyCheckAlt} className="icon" />
-          &nbsp;<b className="text-color">Pending Transactions</b>
+          <FontAwesomeIcon icon={faUserClock} className="icon" />
+          &nbsp;<b className="text-color">Pending Customers</b>
         </Link>
       </li>
-      <li className="nav-item">
+      <li className="nav-item card">
+        <Link
+          to="/admin/customer/pendingAccounts"
+          className="nav-link active"
+          aria-current="page"
+        >
+          <FontAwesomeIcon icon={faUserClock} className="icon" />
+          &nbsp;<b className="text-color">Wallet ApprovedRequest</b>
+        </Link>
+      </li>
+      <li className="nav-item card">
         <Link
           to="/admin/customer/transaction/success"
           className="nav-link active"
@@ -111,17 +81,48 @@ const AdminHeader = () => {
           &nbsp;<b className="text-color">Success Transactions</b>
         </Link>
       </li>
-      <li className="nav-item">
+      <li className="nav-item card">
         <Link
-          to="/Admin/ticket/detail/AdminTicket"
+          to="/admin/customer/transaction/pending"
           className="nav-link active"
           aria-current="page"
         >
-          <FontAwesomeIcon icon={faTicketAlt} className="icon" />
-          &nbsp;<b className="text-color">User Tickets</b>
+          <FontAwesomeIcon icon={faMoneyCheckAlt} className="icon" />
+          &nbsp;<b className="text-color">Pending Transactions</b>
         </Link>
       </li>
-      <li className="nav-item">
+      <li className="nav-item card">
+          <Link
+            to="/Admin/Currency/AdminAccount"
+            className="nav-link active"
+            aria-current="page"
+          >
+            <FontAwesomeIcon icon={faCheckCircle} className="icon" />
+            &nbsp;<b className="text-color">Admin BankAccount</b>
+          </Link>
+        </li>
+      
+        <li className="nav-item card">
+        <Link
+          to="/Admin/Currency/CommonBankAccounts"
+          className="nav-link active"
+          aria-current="page"
+        >
+          <FontAwesomeIcon icon={faCheckCircle} className="icon" />
+          &nbsp;<b className="text-color">Common BankAccounts</b>
+        </Link>
+      </li>
+      <li className="nav-item card">
+          <Link
+            to="/Admin/Currency/EditHostDetailsPage"
+            className="nav-link active"
+            aria-current="page"
+          >
+            <FontAwesomeIcon icon={faCheckCircle} className="icon" />
+            &nbsp;<b className="text-color">Hosting Details </b>
+          </Link>
+        </li>
+        <li className="nav-item card">
         <Link
           to="/Admin/Currency/AddCurrency"
           className="nav-link active"
@@ -131,36 +132,36 @@ const AdminHeader = () => {
           &nbsp;<b className="text-color">Add Currency</b>
         </Link>
       </li>
-      <li className="nav-item">
+      <li className="nav-item card">
         <Link
-          to="/Admin/Currency/CommonBankAccounts"
+          to="/admin/fee/detail/view"
           className="nav-link active"
           aria-current="page"
         >
-          <FontAwesomeIcon icon={faCheckCircle} className="icon" />
-          &nbsp;<b className="text-color">Common Bank Accounts</b>
+          <FontAwesomeIcon icon={faEye} className="icon" />
+          &nbsp;<b className="text-color">View FeeDetails</b>
         </Link>
       </li>
-      <li className="nav-item">
-          <Link
-            to="/Admin/Currency/AdminAccount"
-            className="nav-link active"
-            aria-current="page"
-          >
-            <FontAwesomeIcon icon={faCheckCircle} className="icon" />
-            &nbsp;<b className="text-color">Admin Account</b>
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link
-            to="/Admin/Currency/EditHostDetailsPage"
-            className="nav-link active"
-            aria-current="page"
-          >
-            <FontAwesomeIcon icon={faCheckCircle} className="icon" />
-            &nbsp;<b className="text-color">Hosting  Details </b>
-          </Link>
-        </li>
+      <li className="nav-item card">
+        <Link
+          to="/admin/fee/detail/email"
+          className="nav-link active"
+          aria-current="page"
+        >
+          <FontAwesomeIcon icon={faEnvelope} className="icon" />
+          &nbsp;<b className="text-color">Email Templates</b>
+        </Link>
+      </li>
+      <li className="nav-item card">
+        <Link
+          to="/Admin/ticket/detail/AdminTicket"
+          className="nav-link active"
+          aria-current="page"
+        >
+          <FontAwesomeIcon icon={faTicketAlt} className="icon" />
+          &nbsp;<b className="text-color">User Tickets</b>
+        </Link>
+      </li>
       <ToastContainer />
     </ul>
   );
